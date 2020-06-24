6aed0a5c48de4d3df2cca4473cdcf292197d698b
logantner <logangantner@gmail.com>
2020-06-24 10:09:37 -0400

    Made rankings pull from store and change on tab

--- src/components/rankings/rankings-body.js
+++ src/components/rankings/rankings-body.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-export default function RankingsBody(props) {
+function RankingsBody(props) {
+    const attribute = props.activeTab;
+    const rankings = props[`rankings_${attribute}`]
+    const userKey = attribute === 'total' ? 'username' : 'displayName';
+    const scoreKey = attribute === 'total' ? 'totalLevel' : attribute;
+    console.log(rankings);
+
+    const rows = [];
+    for (let idx=0; idx < rankings.length; ++idx) {
+        let row = rankings[idx];
+        rows.push(
+            <tr>
+            <th scope="row">{idx+1}</th>
+            <th scope="row">{row[userKey]}</th>
+            <th scope="row">{row[scoreKey]}</th>
+            </tr>
+        );
+    }
+
     return (
         <div id="content" className="container-fluid">
-            {/* <div className="container-fluid"> */}
-                <div className="row h-100">
-                    <div className=" ranking-table-window overflow-auto">
-                        {/* <a href="/" className="btn btn-success" id="menu-toggle">Toggle Menu</a> */}
-                        The active tab is: {props.activeTab}
-                    </div>
+            <div className="row h-100">
+                <div className="col pre-scrollable ranking-table-window">
+                    The active tab is: {attribute}
+                    <table className="table table-hover table-dark ranking-table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Rank</th>
+                            <th scope="col">Character</th>
+                            <th scope="col">{attribute} Level</th>
+                            </tr>
+                        </thead>
+                        <tbody>{rows}</tbody>
+                    </table>
                 </div>
-            {/* </div> */}
+            </div>
         </div>
     )
 }
+
+function mapStateToProps(store) {
+    return {
+        rankings_total : store.rankingsTotal,
+        rankings_attack : store.rankingsAttack,
+        rankings_strength : store.rankingsStrength,
+        rankings_defence : store.rankingsDefence,
+    };
+}
+
+export default connect(mapStateToProps)(RankingsBody);
+
