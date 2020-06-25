02be9febdb6af23779d214fe36fc06ac43a530fa
logantner <logangantner@gmail.com>
2020-06-25 06:39:13 -0400

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Testing App", () => {
+  const wrapper = shallow(<App />);
+
+  test("renders a single child element", () => {
+    expect(wrapper.children().length).toBe(1);
+  });
+
+  test("renders 3 routes", () => {
+    expect(wrapper.find("Route").length).toBe(3);
+  });
+
+  test("check '/' route", () => {
+    expect(wrapper.find("Route[path='/']").length).toBe(1);
+  });
+
+  test("check '/login' route", () => {
+    expect(wrapper.find("Route[path='/login']").length).toBe(1);
+  });
+
+  test("check '/directory' corresponding to MotorcycleTable", () => {
+    expect(
+      wrapper.find("Route[path='/directory']").contains("MotorcycleTable")
+    );
+  });
 });
